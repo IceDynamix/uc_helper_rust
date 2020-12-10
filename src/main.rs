d5b457907a29a513213f6f7c31d0c695c8d5ff69
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2020-12-10 01:16:07 +0100

    Add rank history structure

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // let settings = Settings::from_profile("debug").unwrap();
     // let sheet = Sheet::new(settings.staff_sheet_id).await?;
     // let data = tetrio::User::request(vec!["icedynamix".to_string(), "electroyan".to_string()]).await?;
-    let rank_histories = tenchi::PlayerHistory::request().await?;
-    println!("{:?}", rank_histories);
+
+    // Downloads a few gigabytes of data so use with care
+    // let rank_histories = tenchi::PlayerHistory::refresh().await?;
+    let player_history = tenchi::PlayerHistory::from_cache().await?;
+    let username = "icedynamix";
+    println!("all: {:?}", player_history.get_ranks(username).await);
+    println!(
+        "highest: {:?}",
+        player_history
+            .get_ranks(username)
+            .await
+            .unwrap()
+            .iter()
+            .max()
+    );
     Ok(())
 }
