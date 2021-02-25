e66ac559c0b4f0acbb236224c1f3a903f12d54e2
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-02-25 17:11:33 +0100

    reset EVERYTHING

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,9 @@
 
 use std::error::Error;
 
-mod discord;
-mod settings;
-mod tetrio;
-
-pub mod database;
-#[cfg(test)]
-mod tests;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
-
-    // let settings = settings::Settings::from_profile("debug").unwrap();
-
-    // Downloads a few gigabytes of data so use with care
-    // tetrio::tenchi::HighestRanks::refresh().await?;
-    discord::start().await?;
-
+    println!("Hello World");
     Ok(())
 }
